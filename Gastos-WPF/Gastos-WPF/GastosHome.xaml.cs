cda02b6a08b6f587a96a26097544cbf60ccc9da3
Patricia Burgos <patriciaburgos.1999@gmail.com>
2020-02-20 19:39:59 +0100

    Primera App WPF

---
+++ Gastos-WPF/Gastos-WPF/GastosHome.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Gastos_WPF
+{
+    /// <summary>
+    /// Lógica de interacción para GastosHome.xaml
+    /// </summary>
+    public partial class GastosHome : Page
+    {
+        public GastosHome()
+        {
+            InitializeComponent();
+        }
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			// View Expense Report
+			GastosReportPagina expenseReportPage = new GastosReportPagina(this.peopleListBox.SelectedItem);
+			this.NavigationService.Navigate(expenseReportPage);
+		}
+	}
+}
---
+++ Gastos-WPF/Gastos-WPF/GastosReportPagina.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Gastos_WPF
+{
+	/// <summary>
+	/// Lógica de interacción para GastosReportPagina.xaml
+	/// </summary>
+	public partial class GastosReportPagina : Page
+	{
+		public GastosReportPagina()
+		{
+			InitializeComponent();
+		}
+
+		// Custom constructor to pass expense report data
+		public GastosReportPagina(object data) : this()
+		{
+			// Bind to expense report data.
+			this.DataContext = data;
+		}
+	}
+}
